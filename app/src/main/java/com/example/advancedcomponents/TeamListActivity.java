04ecfd3e6440f29d67d44c03cc97415db9d2bf2d
d pullaiah <pullaiah.dudekula@improve10x.com>
2022-11-10 16:08:33 +0530

    completed the functionality of delete

--- app/src/main/java/com/example/advancedcomponents/TeamListActivity.java
+++ app/src/main/java/com/example/advancedcomponents/TeamListActivity.java
@@ -7,6 +7,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -23,15 +24,22 @@ public class TeamListActivity extends AppCompatActivity {
         setUpData();
         setupListview();
         handleAdd();
+        handleDelete();
     }
 
     public void setupListview() {
-         listViewLv = findViewById(R.id.listview_lv);
+        listViewLv = findViewById(R.id.listview_lv);
         arrayAdapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, membersList);
         listViewLv.setAdapter(arrayAdapter);
-
+        listViewLv.setOnItemClickListener((adapterView, view, position, l) -> {
+            Toast.makeText(this, "Clicked : " + membersList.get(position), Toast.LENGTH_SHORT).show();
+            String memberName = membersList.get(position);
+            EditText enterNameTxt = findViewById(R.id.enter_name_txt);
+            enterNameTxt.setText(memberName);
+        });
     }
+
     public void setUpData() {
         membersList = new ArrayList<>();
         membersList.add("Viswanath");
@@ -43,10 +51,21 @@ public class TeamListActivity extends AppCompatActivity {
         addBtn.setOnClickListener(view -> {
             EditText enterNameTxt = findViewById(R.id.enter_name_txt);
             String name = enterNameTxt.getText().toString();
-            if(name.equals("") == false) {
+            if (name.equals("") == false) {
                 arrayAdapter.add(name);
                 arrayAdapter.notifyDataSetChanged();
             }
+            enterNameTxt.setText("");
+        });
+    }
+
+    public void handleDelete() {
+        Button deleteBtn = findViewById(R.id.delete_btn);
+        deleteBtn.setOnClickListener(view -> {
+            EditText enterNameTxt = findViewById(R.id.enter_name_txt);
+            String name = enterNameTxt.getText().toString();
+            arrayAdapter.remove(name);
+            arrayAdapter.notifyDataSetChanged();
             enterNameTxt.setText("");
         });
     }
